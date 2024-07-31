9d6f7a17f18d00f121881ca676264b906bb0812f
ireemli <118290036+ireemli@users.noreply.github.com>
2024-07-31 11:58:15 +0300

    Update loginPage.js
    added login func

--- loginPage.js
+++ loginPage.js
@@ -2,5 +2,20 @@ class LoginPage{
     visit(){
         cy.visit('https://magento.softwaretestingboard.com');
     }
+    signIn(){
+        cy.get('a').contains('Sign In').click();
+      }  
+      
+      enterEmail(email){
+        cy.get("[id='email']").type("iremhverdi@gmail.com")
+      }
+      
+      enterPassword(password){
+        cy.get("[id='pass']").type("929568Ab")
+      } 
+      
+      login(){
+        cy.get("span").contains("Sign In").click();
+      }
 }
 export default LoginPage;
